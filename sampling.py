8f98357bd0b88fd1efc671af72c3ac5dae31ae47
Big3ppvv <joaquinruizperez2002@gmail.com>
2023-08-16 13:11:31 -0300

    XD

--- sampling.py
+++ sampling.py
@@ -13,4 +13,8 @@ print(prob_marginales)
 inferencia_2 = BayesianModelSampling(red_bayes)
 muestras_2 = inferencia_2.forward_sample(size=10000)
 muestras_envidencia = muestras_2[muestras_2['Tren'] == 'demorado']
-muestras_envidencia_multiple  = muestras_2[(muestras_2['Tren'] == 'demorado')and(muestras_2['Lluvia'] == 'fuerte')]
+#muestras_envidencia_multiple  = muestras_2[(muestras_2['Tren'] == 'demorado')and(muestras_2['Lluvia'] == 'ligera')]
+
+ocurrencias = muestras_envidencia['Reunion'].value_counts(normalize=True)
+print()
+print(ocurrencias)
